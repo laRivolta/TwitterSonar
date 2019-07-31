373ccde70f4fbf54c0165061466c11b470868e8f
Fran Mosteiro <fmosteiro@theinit.com>
2019-07-31 18:59:03 +0200

    first commit

---
+++ src/test/java/io/larivolta/twitterSonar/TwitterSonarTestCase.java
@@ -0,0 +1,13 @@
+package io.larivolta.twitterSonar;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TwitterSonarTestCase {
+
+    @Test
+    public void sholud_be_truthy() {
+        assertTrue(true);
+    }
+
+}
