ab04283ee6eea958a9333d906bdca6adddff3a37
Marta Manso <ladymm@gmail.com>
2019-08-04 20:23:36 +0200

    Writing to a CSV in the needed format to generate excel graphs

--- src/main/java/io/larivolta/twitterSonar/TwitterSearcher.java
+++ src/main/java/io/larivolta/twitterSonar/TwitterSearcher.java
@@ -1,6 +1,11 @@
 package io.larivolta.twitterSonar;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -27,24 +32,54 @@ public class TwitterSearcher {
 	 * The userActivities list is composed of: - A list of tweets, retweets and
 	 * replies (max 20) + a list of likes (max 20)
 	 */
-	public void printUserTweetsSince(String screenName, int minutes) {
+	public void printUserTweetsSince(String screenName, Date actualHour, int minutes) {
 		try {
-			getUserActivitiesSince(screenName, minutes).forEach(tweet -> {
+			List<TwitterActivity> userActivities = getUserTweetsSince(screenName, minutes);
+			userActivities.forEach(tweet -> {
 				System.out.println("@" + tweet.getScreen_name() + " - " + tweet.getCreated_at().toString() + " - "
 						+ tweet.getText());
-
 			});
+
 		} catch (Exception e) {
 			System.out.println("Failed to search tweets: " + e.getMessage());
 		}
 	}
 
-	public List<TwitterActivity> getUserActivitiesSince(String screenName, int minutes) {
+	public String[] getUserActivitiesCompilationsSince(String screenName, Date actualHour, int minutes) {
+		String[] stringifiedUserActivities = new String[3];
 		List<TwitterActivity> userActivities = new ArrayList<TwitterActivity>(0);
 		userActivities.addAll(getUserTweetsSince(screenName, minutes));
 		userActivities.addAll(getUserLikesSince(screenName, minutes));
+
 		System.out.println(screenName + " == " + userActivities.size());
-		return userActivities;
+		stringifiedUserActivities[0] = screenName;
+		stringifiedUserActivities[1] = actualHour.toString();
+		stringifiedUserActivities[2] = String.valueOf(userActivities.size());
+
+		return stringifiedUserActivities;
+	}
+
+	/**
+	 * Generates a file with the needed format for graphs: [Datetime, user1, user2,
+	 * user3] [04/08/19 13:00, 2, 0, 6]
+	 * 
+	 * @param actualHour
+	 * @param stringifiedActivities
+	 * @return
+	 */
+	public String[] getCompiledData(Date actualHour, List<String[]> stringifiedActivities) {
+		String[] compiledData = new String[stringifiedActivities.size() + 1];
+		String[] headers = new String[stringifiedActivities.size() + 1];
+		String strDate = new SimpleDateFormat("dd/MM/yy HH:mm").format(actualHour);
+		headers[0] = "Fecha";
+		compiledData[0] = strDate;
+		for (int i = 0; i < stringifiedActivities.size(); i++) {
+			headers[i + 1] = stringifiedActivities.get(i)[0]; // username
+			compiledData[i + 1] = stringifiedActivities.get(i)[2]; // userActivities
+		}
+		System.out.println(Arrays.toString(headers));
+		System.out.println(Arrays.toString(compiledData));
+		return compiledData;
 	}
 
 	/**
@@ -91,6 +126,29 @@ public class TwitterSearcher {
 		}
 
 		return userLikes;
+	}
+
+	public void writeToCsvFile(String[] strings, String separator) {
+
+		File csvFile = new File("/Users/martamanso/twittersonar.csv");
+		if (csvFile.isFile()) {
+
+			try (FileWriter writer = new FileWriter(csvFile, true)) {
+				writer.append(System.lineSeparator());
+				for (int i = 0; i < strings.length; i++) {
+					writer.append(strings[i]);
+					if (i < (strings.length - 1))
+						writer.append(separator);
+				}
+
+				writer.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("ERROR: No Existe fichero");
+		}
+
 	}
 
 }
--- src/main/java/io/larivolta/twitterSonar/TwitterSonar.java
+++ src/main/java/io/larivolta/twitterSonar/TwitterSonar.java
@@ -1,17 +1,30 @@
 package io.larivolta.twitterSonar;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 public class TwitterSonar {
 
 	public static void main(String[] args) {
 
-		int minutes = 60;
-		String usernames[] = new String[] { "MartaMans0", "fran_mosteiro", "zenekezene", "ziraco", "vgaltes" };
+		int minutes = 30;
 		TwitterSearcher ts = new TwitterSearcher();
-		System.out.println("In the last " + minutes + "\':");
+		String usernames[] = new String[] { "MartaMans0", "fran_mosteiro", "zenekezene", "ziraco", "vgaltes" };
+		System.out.println("Activity in the last " + minutes + "\':");
+
+		Calendar cal = Calendar.getInstance();
+		Date actualHour = cal.getTime();
+
+		List<String[]> stringifiedActivities = new ArrayList<String[]>(0);
 		for (int i = 0; i < usernames.length; i++) {
 
-			ts.printUserTweetsSince(usernames[i], minutes);
+			stringifiedActivities.add(ts.getUserActivitiesCompilationsSince(usernames[i], actualHour, minutes).clone());
 
 		}
+		String[] s = ts.getCompiledData(actualHour, stringifiedActivities);
+		ts.writeToCsvFile(s, ",");
+
 	}
 }
